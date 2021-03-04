268feb40600e2fef88404accf974ad9eb5cd1d50
Paul ALNET <paul@shimaore.net>
2021-03-04 13:54:49 +0100

    Add ability to remove track file

--- routes/track.js
+++ routes/track.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 const spotify = require('../services/spotify')
 const jobs = require('../services/jobs')
+const remove = require('../services/remove')
 
 // HTML page with track info
 router.get('/:id/', async (req, res) => {
@@ -21,6 +22,18 @@ router.get('/:id/queue', async (req, res) => {
   await jobs.addSpotifyJob(req.params.id)
 
   res.redirect(`/track/${req.params.id}/`)
+})
+
+router.get('/:id/remove', async (req, res) => {
+  if (!req.params.id) return res.end('no id???')
+
+  try {
+    await remove.track(req.redis, req.params.id)
+    res.redirect(`/track/${req.params.id}/`)
+  } catch (err) {
+    console.log(err)
+    res.status(500).end('Failed. May be an invalid ID')
+  }
 })
 
 module.exports = router
