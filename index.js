33e6b205c72fc0201e64ecdb3826e0187d10ada7
Paul ALNET <paul@shimaore.net>
2021-03-13 21:54:51 +0100

    Remove breaking sockets

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const app = express()
 const http = require('http').Server(app)
-const io = require('socket.io')(http)
-const socketRouter = require('./socket/router.js')
 const router = require('./routes/index')
 const bodyParser = require('body-parser')
 
@@ -11,11 +9,6 @@ app.set('view engine', 'pug')
 app.use('/', express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', router)
-
-io.on('connection', (socket) => {
-	console.log('a user connected')
-	socketRouter.route(socket)
-})
 
 http.listen(3000, () => {
   console.log('listening on *:3000')
