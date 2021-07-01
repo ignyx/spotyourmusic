ignyx/spotyourmusic

commit 6f83111956bc0bf8974339e9de32b7c6da44ae1d

Add feed episode status to feed page

routes/controllers/rss.js
@@ -33,10 +33,23 @@ module.exports.removeEpisode = async (redis, feed, jobId) => {
 module.exports.getFeed = async (redis, feed) => {
   let episodes = await redis.lrange('feed' + feed, 0, 1000)
   let pipeline = redis.pipeline()
+  // Fetch episodes
   episodes.forEach(episodeId => {
     if (episodeId) pipeline.hgetall('episode' + episodeId)
   })
-  return Array.from(await pipeline.exec(), result => result[1])
+  let feed = Array.from(await pipeline.exec(), result => result[1])
+
+  // Fetch job status
+  let jobPipeline = redis.pipeline()
+  feed.forEach((episode) => {
+    jobPipeline.hgetall('job' + episode.jobId)
+  });
+
+  (await jobPipeline.exec()).forEach((job, i) => {
+    feed[i].job = job
+  });
+
+  return feed
 }
 
 // Fetches all feeds

views/feed.pug
@@ -6,7 +6,7 @@ block content
 
   form(method='POST')
     h3 Add Episode
-    label(for='videoId') Youtube Video Id or url : 
+    label(for='videoId') Youtube Video Id or url :
     input(type='text' id='videoId' name='videoId')
     input(type='submit')
 
@@ -19,6 +19,7 @@ block content
         th Title
         th Added
         th Job
+        th Job status
       each episode in episodes
         tr
           td
@@ -28,5 +29,6 @@ block content
           td #{episode.dateAdded}
           td
             a(href=`/jobs/${episode.jobId}/`) #{episode.jobId}
+          td #{episode.job.status}
   else
     p No episodes :( Maybe add some?

commit 5a0466835966512afbb6824654fb0979433a605d

Fix declaration issue caused by 6f83111956bc0bf8974339e9de32b7c6da44ae1d

routes/controllers/rss.js
@@ -37,16 +37,16 @@ module.exports.getFeed = async (redis, feed) => {
   episodes.forEach(episodeId => {
     if (episodeId) pipeline.hgetall('episode' + episodeId)
   })
-  let feed = Array.from(await pipeline.exec(), result => result[1])
+  let feedEpisodes = Array.from(await pipeline.exec(), result => result[1])
 
   // Fetch job status
   let jobPipeline = redis.pipeline()
-  feed.forEach((episode) => {
+  feedEpisodes.forEach((episode) => {
     jobPipeline.hgetall('job' + episode.jobId)
   });
 
   (await jobPipeline.exec()).forEach((job, i) => {
-    feed[i].job = job
+    feedEpisodes[i].job = job
   });
 
   return feed